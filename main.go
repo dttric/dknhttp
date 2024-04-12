0d778cb95b6b5a8f6a8e560388e1d615ac66b669
dttric <87319664+dttric@users.noreply.github.com>
2024-04-13 00:57:56 +0700

    конец

--- main.go
+++ main.go
@@ -3,33 +3,46 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 	"os"
-	// "github.com/julienschmidt/httprouter"
+
+	"github.com/julienschmidt/httprouter"
 )
 
 type responseData map[string]interface{}
 
-func Home(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "СТРАНИЦА БЕЗ HTML")
+func loadTemplate(tpl string) *template.Template {
+	return template.Must(template.ParseFiles(fmt.Sprintf("%s", tpl)))
 }
 
-func loadTemplate(tpl string) *template.Template {
-  return template.Must(template.ParseFiles(fmt.Sprintf(tpl)))
+func contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
 }
 
 func main() {
-	cfg, _ := os.ReadFile("./config.json")
-	fmt.Println(string(cfg))
-	// portNum := ":" + string(cfg.port)
+	args := os.Args
+	if contains(args, "-h") || contains(args, "-r") {
+		if args[1] == "-h" {
+			fmt.Println("-h - помощь\n-r <./путь/до/файла.html> <порт> - запуск сервера")
+		} else if args[1] == "-r" {
+			portNum := ":" + string(args[3])
+			router := httprouter.New()
 
-	// router := httprouter.New()
+			router.GET("/", func(response http.ResponseWriter, request *http.Request, params httprouter.Params) {
+				_ = loadTemplate(args[2]).Execute(response, responseData{})
+			})
 
-	// router.GET(cfg.path, func(response http.ResponseWriter, request *http.Request, params httprouter.Params) {
-	// 	_ = loadTemplate(cfg.path[1:]).Execute(response, responseData{})
-	// })
-
-  // _ = http.ListenAndServe(portNum, router)
-	// log.Println("Запущен на порте", portNum)
-  // fmt.Println("Для разноса сервера CTRL+C")
+			log.Println("Запущен на порте", portNum)
+			fmt.Println("Для разноса сервера CTRL+C")
+			_ = http.ListenAndServe(portNum, router)
+		}
+	} else {
+		fmt.Println("Введите -h!")
+	}
 }
